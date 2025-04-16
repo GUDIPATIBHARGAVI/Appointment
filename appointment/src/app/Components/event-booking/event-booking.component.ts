a3021a29244073db0d519b7969ee17756870a79e
GUDIPATI BHARGAVI <gudipatibhargavi182@gmail.com>
2025-04-16 21:59:31 +0530

    added components

---
+++ appointment/src/app/Components/event-booking/event-booking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-booking',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-booking.component.html',
+  styleUrl: './event-booking.component.css'
+})
+export class EventBookingComponent {
+
+}
